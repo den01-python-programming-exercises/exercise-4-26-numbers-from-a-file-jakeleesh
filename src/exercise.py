6c83a283f6b6773777d2e5c30164eab172645fc3
Jake Lee <jake.lee@fdmgroup.com>
2021-10-17 16:11:21 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
-
+    file_name = input("File?")
+    lower = int(input("Lower bound?"))
+    upper = int(input("Upper bound?"))
+    count = 0
+    try:
+        f = open(file_name, "r")
+        data = f.read().splitlines()
+        for num in data:
+            if (int(num) >= lower) and (int(num) <= upper):
+                count += 1
+        print("Numbers: " + str(count))
+    except:
+        print("Error")
 if __name__ == '__main__':
     main()
